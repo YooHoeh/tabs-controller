4d2dd278a5d05c830af65448d53bf191b359dbab
YooHoeh <zako2810@sina.com>
2019-03-29 19:17:45 +0800

    edit

--- TabsController.js
+++ TabsController.js
@@ -1,11 +1,14 @@
 class TabsController {
   constructor(element) {
     this.ele = element;
+    this.init();
   }
   init() {
-    this.ele.querySelector(".tab-bar").array.forEach(i => {
+    Array.prototype.forEach.call(this.ele.querySelector(".tab-bar"), i => {
       classList.remove("active");
     });
-    this.ele.querySelector(".tab-bar").childNodes[0].classList.add("active");
+    this.ele
+      .querySelector(".tab-bar")
+      .childNodes[0].classList.addClassf("active");
   }
 }
