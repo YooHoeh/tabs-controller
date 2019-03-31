abd513ad4399bbabd3d90aa1890a3b246da43cab
YooHoeh <zako2810@sina.com>
2019-03-31 11:14:38 +0800

    finish demo

--- TabsController.js
+++ TabsController.js
@@ -1,18 +1,29 @@
 class TabsController {
   constructor(element) {
-    this.nav = element.querySelector(".tab-bar");
-    this.content = element.querySelector(".tab-content");
+    this.nav = element.querySelector(".tab-bar").children;
+    this.content = element.querySelector(".tab-content").children;
+    this.active = 1;
     this.init();
-    this.bindEvent();
+    this.bindEvents();
   }
   init() {
-    this.nav.children[0].classList.add("active");
-    this.content.children[0].classList.add("active");
+    this.nav[0].classList.add("active");
+    this.content[0].classList.add("active");
   }
-  bindEvent() {
-    console.log(this.nav.length);
-    Array.prototype.forEach.call(this.nav, i => {
-      classList.remove("active");
+  bindEvents() {
+    const forEach = (context, fn) => Array.prototype.forEach.call(context, fn);
+    const indexOf = (context, item) =>
+      Array.prototype.indexOf.call(context, item);
+
+    forEach(this.nav, i => {
+      i.addEventListener("click", e => {
+        forEach(this.nav, i => i.classList.remove("active"));
+        forEach(this.content, i => i.classList.remove("active"));
+        this.active = indexOf(this.nav, e.target);
+        this.nav[this.active].classList.add("active");
+        this.content[this.active].classList.add("active");
+        console.log(this.active);
+      });
     });
   }
 }
